8f0d140b84bdf41a3e8e175701fdcdce3d6bd4af
Max Furtuna <ekstazyst@gmail.com>
2020-04-05 14:00:10 +0300

    Update docs and export

--- src/ConnectionFactory.php
+++ src/ConnectionFactory.php
@@ -5,9 +5,13 @@ namespace ekstazi\websocket\stream;
 use Amp\Promise;
 use Psr\Http\Message\RequestInterface;
 
+/**
+ * Interface ConnectionFactory
+ * @package ekstazi\websocket\stream
+ */
 interface ConnectionFactory
 {
-    /**
+    /**1
      * Send binary frames.
      */
     const MODE_BINARY = 'binary';
@@ -17,5 +21,11 @@ interface ConnectionFactory
      */
     const MODE_TEXT = 'text';
 
+    /**
+     * Create websocket stream for given request and frame mode
+     * @param RequestInterface $request the request used to create websocket connection
+     * @param string $mode Mode to use one of self::MODE_BINARY or self::MODE_TEXT
+     * @return Promise<Stream> The promise that succeeds with stream for websocket connection
+     */
     public function connect(RequestInterface $request, string $mode = self::MODE_BINARY): Promise;
 }
--- src/Stream.php
+++ src/Stream.php
@@ -5,6 +5,10 @@ namespace ekstazi\websocket\stream;
 use Amp\ByteStream\InputStream;
 use Amp\ByteStream\OutputStream;
 
+/**
+ * Interface Stream
+ * @package ekstazi\websocket\stream
+ */
 interface Stream extends InputStream, OutputStream
 {
 }
